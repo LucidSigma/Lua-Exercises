d19578d0d85ba12e79a2caad50728ef05180cea1
Matt Schafer <lucidsigma17@gmail.com>
2019-05-15 16:26:20 +1000

    Markov Chain.
    Added a Markov Chain algorithm.

--- Lua Exercises/iterators.lua
+++ Lua Exercises/iterators.lua
@@ -52,3 +52,80 @@ for w in all_unique_words()
 do
 	print(w)
 end
+
+function markov_chain()
+	function allwords()
+		local line = io.read()
+		local position = 1
+		
+		return function()
+			while line ~= nil
+			do
+				local word, end_index = string.match(line, "(%w+[,;.:]?()", position)
+				
+				if word ~= nil
+				then
+					position = end_index
+					
+					return word
+				else
+					line = io.read()
+					position = 1
+				end
+			end
+			
+			return nil
+		end
+	end
+
+	function prefix(word1, word2)
+		return w1 .. " " .. w2
+	end
+
+	local statetab = { }
+
+	function insert(prefix, value)
+		local list = statetab[prefix]
+		
+		if list == nil
+		then
+			statetab[prefix] = { value }
+		else
+			list[#list + 1] = value
+		end
+	end
+
+	local MAX_GEN = 200
+	local NO_WORD = "\n"
+
+	local word1, word2 = NO_WORD, NO_WORD
+
+	for next_word in allwords()
+	do
+		insert(prefix(word1, word2), NO_WORD)
+		word1 = word2
+		word2 = next_word
+	end
+
+	insert(prefix(word1, word2), NO_WORD)
+
+	word1 = NO_WORD
+	word2 = NO_WORD
+
+	for i = 1, MAX_GEN
+	do
+		local list = statetab[prefix(word1, word2)]
+		local random_item = math.random(#list)
+		local next_word = list[random_item]
+		
+		if next_word == NO_WORD
+		then
+			return
+		end
+		
+		io.write(next_word, " ")
+		
+		word1 = word2
+		word2 = next_word
+	end
+end
